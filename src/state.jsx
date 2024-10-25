7f73e4d881feb02903b37ff8d87c32f2c87bd4ae
Sergio Chizhikov <chchizh@gmail.com>
2024-10-25 12:18:35 +0300

    change eslint, add user

---
+++ src/state.jsx
@@ -0,0 +1,25 @@
+const user = {
+  name: "Иван",
+  lastname: "Иванов",
+  id: "1",
+  email: "ivan@mail.io",
+  about: "Тут я рассказываю интересную историю",
+  avatar:
+    "https://img.freepik.com/free-psd/3d-illustration-bald-person-with-glasses_23-2149436184.jpg",
+};
+
+const users = {
+  0: { name: "Валерия", lastname: "Смирнова" },
+  1: { name: "Павел", lastname: "Николаев" },
+  2: { name: "Владислав", lastname: "Тетерин" },
+  3: { name: "Юлия", lastname: "Петрова" },
+  4: { name: "Ксения", lastname: "Иванова" },
+};
+
+export function getUser() {
+  return user;
+}
+
+export function getUsers() {
+  return users;
+}
