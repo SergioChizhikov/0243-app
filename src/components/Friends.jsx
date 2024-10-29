9b0b7f580addaa8f4c393e14ed094a5af70e863a
Sergio Chizhikov <chchizh@gmail.com>
2024-10-29 10:27:38 +0300

    call server, add class Friends

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import friendsImg from "../assets/img/img.webp";
+import React from "react";
 
 const TableRow = (props) => {
   return (
@@ -14,8 +16,50 @@ const TableRow = (props) => {
   );
 };
 
+export class Friends extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userRow: [] };
+  }
+
+   componentDidMount() {
+    
+   }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row mb-5">
+          <div className="col-12">
+            <h3>Список друзей</h3>
+            <p>Дополнительная информация</p>
+            <img src={friendsImg} alt="" width="100%" />
+          </div>
+        </div>
+        <div className="row">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Имя и фамилия</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 export const Friends = (props) => {
   let users = props.function();
+  console.log(users);
   let usersCount = Object.keys(users).length;
   let userRow = [];
 
@@ -23,28 +67,5 @@ export const Friends = (props) => {
     userRow.push(<TableRow key={i} index={i} id={users[i].id} name={users[i].name} lastname={users[i].lastname} email={users[i].email}/>);
   }
 
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12">
-          <h3>Список друзей</h3>
-          <p>Дополнительная информация</p>
-        </div>
-      </div>
-      <div className="row">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Имя и фамилия</th>
-              <th scope="col">Email</th>
-                          </tr>
-          </thead>
-          <tbody>
-            {userRow}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+
 };
